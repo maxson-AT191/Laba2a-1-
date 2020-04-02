5e21db1dd8d01816575d20eaac086fd262f7c041
maxson-AT191 <maxlenaruss123@gmail.com>
2020-04-02 15:56:49 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba2a_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Chetiroxygol CHETIROXYGOLNIK = new Chetiroxygol();
+            CHETIROXYGOLNIK.SIZESCHetver();
+            CHETIROXYGOLNIK.GetSizeofside();
+            CHETIROXYGOLNIK.GetDiagonal();
+            CHETIROXYGOLNIK.GetPerimetr();
+            CHETIROXYGOLNIK.GetArea();
+        }
+    }
+}
